5b53ffacb9a7e33bc78c95730c000a51581609f1
Deepak Singh <deepkushwah824@gmail.com>
2023-05-04 23:39:31 +0530

    Add files via upload

---
+++ temp_humid_wifi_dashboard.ino
@@ -0,0 +1,100 @@
+#include "ThingsBoard.h"
+
+#include <WiFi.h>
+
+
+#define WIFI_AP             "Deepak singh"
+#define WIFI_PASSWORD       "rajputanakijay"
+
+// See https://thingsboard.io/docs/getting-started-guides/helloworld/
+// to understand how to obtain an access token
+#define TOKEN               "2NuFSTgk1PGUXjLI1KxV"
+#define THINGSBOARD_SERVER  "demo.thingsboard.io"
+
+// Baud rate for debug serial
+#define SERIAL_DEBUG_BAUD   115200
+#include "DHT.h"
+#define DHT11PIN 15
+// Initialize ThingsBoard client
+WiFiClient espClient;
+// Initialize ThingsBoard instance
+ThingsBoard tb(espClient);
+// the Wifi radio's status
+int status = WL_IDLE_STATUS;
+
+
+DHT dht(DHT11PIN, DHT11);
+void setup() {
+  // initialize serial for debugging
+  Serial.begin(SERIAL_DEBUG_BAUD);
+  WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+  InitWiFi();
+  /* Start the DHT11 Sensor */
+  dht.begin();
+}
+
+void loop() {
+  delay(2500);
+
+  if (WiFi.status() != WL_CONNECTED) {
+    reconnect();
+  }
+
+  if (!tb.connected()) {
+    // Connect to the ThingsBoard
+    Serial.print("Connecting to: ");
+    Serial.print(THINGSBOARD_SERVER);
+    Serial.print(" with token ");
+    Serial.println(TOKEN);
+    if (!tb.connect(THINGSBOARD_SERVER, TOKEN)) {
+      Serial.println("Failed to connect");
+      return;
+    }
+  }
+
+float humi = dht.readHumidity();
+  float temp = dht.readTemperature();
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print("ºC ");
+  Serial.print("Humidity: ");
+  Serial.println(humi);
+//  delay(1000);
+  
+  Serial.println("Sending data...");
+
+  // Uploads new telemetry to ThingsBoard using MQTT.
+  // See https://thingsboard.io/docs/reference/mqtt-api/#telemetry-upload-api
+  // for more details
+
+  tb.sendTelemetryInt("temperature", temp);
+  tb.sendTelemetryFloat("humidity", humi);
+
+  tb.loop();
+}
+
+void InitWiFi()
+{
+  Serial.println("Connecting to AP ...");
+  // attempt to connect to WiFi network
+
+  WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to AP");
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  status = WiFi.status();
+  if ( status != WL_CONNECTED) {
+    WiFi.begin(WIFI_AP, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("Connected to AP");
+  }
+}
